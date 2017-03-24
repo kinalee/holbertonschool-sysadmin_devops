09cf805fd42055cb5c03e3413afd6c5239c9f5e9
Kina Lee <kina.lee@holbertonschool.com>
2017-03-24 23:24:30 +0000

    path

--- 0x13-configuration_management/0-create_a_file.pp
+++ 0x13-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# Using Puppet, create a file in /tmp
-file { '/tmp/holberton':
+# Using Puppet, create a file
+file { 'holberton':
+     path => '/tmp/'
      mode => 0744,
      owner => 'www-data',
      group => 'www-data',
-     content => 'I love Puppet'
+     content => 'I love Puppet',
 }
