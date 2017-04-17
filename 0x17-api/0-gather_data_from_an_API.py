62fa3cd5f12345c3440c479234618a607a2be535
Kina Lee <kina.lee@holbertonschool.com>
2017-04-17 22:20:12 +0000

    made a function

--- 0x17-api/0-gather_data_from_an_API.py
+++ 0x17-api/0-gather_data_from_an_API.py
@@ -5,18 +5,19 @@ Returns infomation about an employee's todo list progress when ID is given
 import sys
 import requests
 
-if __name__ == "__main__":
+def gather_api_data(emId):
     api = 'https://jsonplaceholder.typicode.com'
-    if len(sys.argv) == 2:
-        name = requests.get(api + '/users/' + sys.argv[1]).json().get("name")
-        todos = requests.get(api + '/todos?userId=' + sys.argv[1]).json()
-        completed = requests.get(api + '/todos?completed=true&userId=' +
-                                 sys.argv[1]).json()
+    name = requests.get(api + '/users/' + sys.argv[1]).json().get("name")
+    todos = requests.get(api + '/todos?userId=' + sys.argv[1]).json()
+    completed = requests.get(api + '/todos?completed=true&userId=' +
+                             sys.argv[1]).json()
 
-        print("Employee {} is done with tasks({:d}/{:d}):".
-              format(name, len(completed), len(todos)))
-        for task in completed:
-            print("\t", end="")
-            print(task.get("title"))
-    else:
-        pass
+    print("Employee {} is done with tasks({:d}/{:d}):".
+          format(name, len(completed), len(todos)))
+    for task in completed:
+        print("\t", end="")
+        print(task.get("title"))
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        gather_api_data(sys.argv[1])
