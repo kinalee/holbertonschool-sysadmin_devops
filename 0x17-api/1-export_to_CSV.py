589904f8062864c07c107fd4e17e98132621f94a
Kina Lee <kina.lee@holbertonschool.com>
2017-04-17 23:52:11 +0000

    1

---
+++ 0x17-api/1-export_to_CSV.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Using task #0, export data in the CSV format
+"""
+import sys
+import requests
+import csv
+
+
+if __name__ == "__main__":
+    api = 'https://jsonplaceholder.typicode.com'
+    username = requests.get(api + '/users/' +
+                            sys.argv[1]).json().get("username")
+    todos = requests.get(api + '/users/' + sys.argv[1] +
+                         '/todos').json()
+    filename = "{}.csv".format(sys.argv[1])
+
+    with open(filename, 'w') as cf:
+        writer = csv.writer(cf, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([sys.argv[1], username, task.get("completed"),
+                             task.get("title")])
