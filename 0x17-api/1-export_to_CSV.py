950c3ad50e40044735cd492d0a4e27410d41c9ea
Kina Lee <kina.lee@holbertonschool.com>
2017-04-18 00:02:09 +0000

    only runs when sys.argv[1] is given

--- 0x17-api/1-export_to_CSV.py
+++ 0x17-api/1-export_to_CSV.py
@@ -8,16 +8,17 @@ import csv
 
 
 if __name__ == "__main__":
-    api = 'https://jsonplaceholder.typicode.com'
-    username = requests.get(api + '/users/' +
-                            sys.argv[1]).json().get("username")
-    todos = requests.get(api + '/users/' + sys.argv[1] +
-                         '/todos').json()
-    filename = "{}.csv".format(sys.argv[1])
+    if len(sys.argv) == 2:
+        api = 'https://jsonplaceholder.typicode.com'
+        username = requests.get(api + '/users/' +
+                                sys.argv[1]).json().get("username")
+        todos = requests.get(api + '/users/' + sys.argv[1] +
+                             '/todos').json()
+        filename = "{}.csv".format(sys.argv[1])
 
-    with open(filename, mode='w') as cf:
-        writer = csv.writer(cf, delimiter=',', quotechar='"',
-                            quoting=csv.QUOTE_ALL)
-        for task in todos:
-            writer.writerow([sys.argv[1], username, task.get("completed"),
-                             task.get("title")])
+        with open(filename, mode='w') as cf:
+            writer = csv.writer(cf, delimiter=',', quotechar='"',
+                                quoting=csv.QUOTE_ALL)
+            for task in todos:
+                writer.writerow([sys.argv[1], username, task.get("completed"),
+                                 task.get("title")])
