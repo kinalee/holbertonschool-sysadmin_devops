b60141dd7babef7c52994d933387671bb5c161e2
Kina Lee <kina.lee@holbertonschool.com>
2017-04-18 03:01:07 +0000

    checks given argument

--- 0x17-api/2-export_to_JSON.py
+++ 0x17-api/2-export_to_JSON.py
@@ -8,20 +8,21 @@ import json
 
 
 if __name__ == "__main__":
-    api = 'https://jsonplaceholder.typicode.com'
-    username = requests.get(api + '/users/' +
-                            sys.argv[1]).json().get("username")
-    todos = requests.get(api + '/users/' + sys.argv[1] +
-                         '/todos').json()
+    if len(sys.argv) == 2:
+        api = 'https://jsonplaceholder.typicode.com'
+        username = requests.get(api + '/users/' +
+                                sys.argv[1]).json().get("username")
+        todos = requests.get(api + '/users/' + sys.argv[1] +
+                             '/todos').json()
 
-    taskList = list()
-    for task in todos:
-        tasks = dict()
-        tasks["completed"] = task.get("completed")
-        tasks["task"] = task.get("title")
-        tasks["username"] = username
-        taskList.append(tasks)
+        taskList = list()
+        for task in todos:
+            tasks = dict()
+            tasks["completed"] = task.get("completed")
+            tasks["task"] = task.get("title")
+            tasks["username"] = username
+            taskList.append(tasks)
 
-    filename = "{}.json".format(sys.argv[1])
-    with open(filename, mode='w') as jf:
-        json.dump({sys.argv[1]: taskList}, jf)
+        filename = "{}.json".format(sys.argv[1])
+        with open(filename, mode='w') as jf:
+            json.dump({sys.argv[1]: taskList}, jf)
