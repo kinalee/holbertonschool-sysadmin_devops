539fb2548085d8f5b0723bcdaa59cf1812328f7e
Kina Lee <kina.lee@holbertonschool.com>
2017-04-18 02:29:35 +0000

    2

---
+++ 0x17-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Using task #0, export data in the JSON format
+"""
+import sys
+import requests
+import json
+
+
+if __name__ == "__main__":
+    api = 'https://jsonplaceholder.typicode.com'
+    username = requests.get(api + '/users/' +
+                            sys.argv[1]).json().get("username")
+    todos = requests.get(api + '/users/' + sys.argv[1] +
+                         '/todos').json()
+
+    taskList = list()
+    for task in todos:
+        tasks = dict()
+        tasks["completed"] = task.get("completed")
+        tasks["task"] = task.get("title")
+        tasks["username"] = username
+        taskList.append(tasks)
+
+    filename = "{}.json".format(sys.argv[1])
+    with open(filename, mode='w') as jf:
+        json.dump({sys.argv[1]: taskList}, jf)
